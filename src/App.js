01363d69959ef67ff287c1de1ec34331224a1c8f
Marlon Menegatti <marlonmenegatt@gmail.com>
2020-04-07 00:22:53 -0300

    Thema de estilos globais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import {
+  CssBaseline,
+  ThemeProvider
+} from '@material-ui/core';
+
+import theme from './theme';
 
 export default function App() {
   return(
-    <h1>Pricing</h1>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <h1>Pricing</h1>
+    </ThemeProvider>
   );
 }
